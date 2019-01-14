b35e2e35e5215a4e66bb7af68328e9812730c7fd
topickering <topickering@gmail.com>
2019-01-14 18:59:23 +0000

    Transaction: Added print functions which format transaction for statement

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,9 +2,25 @@ class Transaction
 
   attr_reader :amount, :time
 
-  def initialize(amount)
+  def initialize(amount, time = Time.new)
     @amount = amount
-    @time = Time.new
+    @time = time
+  end
+
+  def transaction_print
+    "#{time_formatter}" + "#{amount_formatter}"
+  end
+
+  def time_formatter
+    "#{@time.day}/#{@time.month}/#{@time.year} ||"
+  end
+
+  def amount_formatter
+    if @amount < 0
+      "  ||  #{-@amount}"
+    else
+      " #{@amount} || "
+    end
   end
 
 end
